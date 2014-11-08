fadcb988ad56107d5e76259d3480bda58e4c824e
Anne <annekao@usc.edu>
2014-11-08 13:43:47 -0800

    how to json ?? ?????

--- FestivalPlay/app/models/Festival.java
+++ FestivalPlay/app/models/Festival.java
@@ -1,15 +1,19 @@
 package models;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import play.db.ebean.Model;
 
 @Entity
-public class Festival extends Model {
+public class Festival extends Model{
 
 	@Id
 	//TicketMaster's id for this given Festival
@@ -27,12 +31,26 @@ public class Festival extends Model {
 	public Festival(String name){
 		this.name = name;
 		System.out.println(name);
+		
 		//send information to TicketMaster and get JSON back to parse
+		String APIkey = "u63kS9dlzruLsznw2xFAhVVgxftOCpBw";
+		String urlBase = "http://app.ticketmaster.com/v1/light/events?apikey=";
+		generateArtist(urlBase+APIkey);
 	}
 	
 	//Parses JSON for artists
-	public void generateArtist(){
-		
+	public void generateArtist(String url){
+		try{
+			JSONParser parser = new JSONParser();
+			Object obj = parser.parse(new FileReader(url));
+			
+			JSONObject jsonObj = (JSONObject) obj;
+			String testing = (String) jsonObj.get("token");
+			System.out.println(testing);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	public void generatePlaylist(){
